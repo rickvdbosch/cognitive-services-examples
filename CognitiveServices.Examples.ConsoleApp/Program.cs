0f214f24847ad234739119ce9943a3b721516f20
Rick van den Bosch <rickvdbosch@outlook.com>
2020-11-19 11:27:55 +0100

    New version of CustomVisionPredictionClient

--- CognitiveServices.Examples.ConsoleApp/Program.cs
+++ CognitiveServices.Examples.ConsoleApp/Program.cs
@@ -11,11 +11,11 @@ namespace CognitiveServices.Examples.ConsoleApp
         {
             // Create a CustomVisionPredictionClient, initialize it with the settings of your 
             // Custom Vision project and send a file over to classify.
-            var client = new CustomVisionPredictionClient
+            var client = new CustomVisionPredictionClient(new ApiKeyServiceClientCredentials(Constants.CV_KEY))
             {
-                ApiKey = Constants.CV_KEY,
                 Endpoint = Constants.ENDPOINT
             };
+            
             using var stream = File.Open("<YOUR_TESTFILE_NAME>", FileMode.Open);
             var prediction = client.ClassifyImage(Constants.PROJECT_ID, Constants.PUBLISHED_NAME, stream);
             foreach (var pred in prediction.Predictions)
